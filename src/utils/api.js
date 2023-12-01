e142ceca77cc754ae6afa552548aa57b1da5d71c
Mubashir Ali Kalhoro <108943166+mubashiralikalhoro@users.noreply.github.com>
2023-12-02 00:06:10 +0500

    added custom api caller

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,20 @@
-import axios from "axios";
-import apiEndPoints from "../constants/apiEndPoints";
+import React from "react";
+import apiCaller from "./apiCaller";
 
-const api = axios.create({
-  baseURL: apiEndPoints.BASE_URL,
-});
+const call = async (type, url = "", options) => {
+  try {
+    const data = await apiCaller[type](url, options);
+    return [data, null];
+  } catch (e) {
+    const message = e?.message || "Something went wrong!";
+    return [null, message];
+  }
+};
+
+const api = {
+  get: async (url, options) => {
+    return await call("get", url, options);
+  },
+};
 
 export default api;
