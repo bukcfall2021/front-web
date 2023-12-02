2d1e4fae3df1833b3de9233e2fc055b42ef2ff1d
Huzaiifa10 <tahamehboob9@gmail.com>
2023-12-02 14:50:55 -0800

    Added assets folder

--- src/router/AppRouting.jsx
+++ src/router/AppRouting.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/home";
 import Navbar from "../components/Navbar";
+import Menu from "../pages/Menu/Menu";
 
 const AppRouting = () => {
   return (
@@ -8,6 +9,7 @@ const AppRouting = () => {
       <Navbar/>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route path="/Menu" element={<Menu/>} />
       </Routes>
     </BrowserRouter>
   );
