0d70083051f2a9be0dd1e828070e3866d487c610
Mubashir Ali Kalhoro <108943166+mubashiralikalhoro@users.noreply.github.com>
2023-12-05 17:54:52 +0500

    Update ToggleButton.jsx

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -2,31 +2,12 @@ import React, { useState } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const ToggleButton = () => {
-  const [isBackClicked, setIsBackClicked] = useState(false);
-  const [isForwardClicked, setIsForwardClicked] = useState(false);
-
-  const handleBackClick = () => {
-    setIsBackClicked(true);
-    setIsForwardClicked(false);
-  };
-
-  const handleForwardClick = () => {
-    setIsBackClicked(false);
-    setIsForwardClicked(true);
-  };
-
   return (
     <div className="flex justify-between items-center m-auto">
-      <div
-        className={`w-10 h-10 rounded-full flex items-center bg-${isBackClicked ? "primary" : "gray-200"}`}
-        onClick={handleBackClick}
-      >
+      <div className={`w-10 h-10 rounded-full flex items-center bg-gray-200`}>
         <IoIosArrowBack className="mx-auto" />
       </div>
-      <div
-        className={`w-10 h-10 rounded-full flex items-center bg-${isForwardClicked ? "primary" : "gray-200"}`}
-        onClick={handleForwardClick}
-      >
+      <div className={`w-10 h-10 rounded-full flex items-center bg-gray-200`}>
         <IoIosArrowForward className="mx-auto" />
       </div>
     </div>
